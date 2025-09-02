95bf3ab061d8314ff96ac6844fc5bda2c48415ea
Kunkawee <kunkawee.s@ku.th>
2025-09-02 17:14:31 +0700

    feat(users): add user module with entity, service, controller and tests

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConcertsModule } from './concerts/concerts.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [ConcertsModule],
+  imports: [ConcertsModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
