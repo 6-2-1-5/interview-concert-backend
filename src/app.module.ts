3695c90b2455cdab80df1744daf174ae03aaabb5
Kunkawee <kunkawee.s@ku.th>
2025-09-03 19:39:13 +0700

    feat(reservations): add reservations module with CRUD operations

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConcertsModule } from './concerts/concerts.module';
 import { UsersModule } from './users/users.module';
+import { ReservationsModule } from './reservations/reservations.module';
 
 @Module({
-  imports: [ConcertsModule, UsersModule],
+  imports: [ConcertsModule, UsersModule, ReservationsModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/reservations/entities/reservation.entity.ts
@@ -0,0 +1,6 @@
+export class Reservation {
+  id: number;
+  userId: number;
+  concertId: number;
+  createdAt: Date;
+}
